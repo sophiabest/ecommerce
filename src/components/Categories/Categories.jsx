9eabd34ca35d487f4497d392d554490c6d4383d3
sophiabest <bestsophia1014@gmail.com>
2022-06-06 18:55:49 -0400

    remove in line styling

--- src/components/Categories/Categories.jsx
+++ src/components/Categories/Categories.jsx
@@ -3,12 +3,6 @@ import { categories } from "../data";
 import CategoryItem from "./CategoryItem";
 import { mobile } from "../responsive";
 
-const Container = styled.div`
-    display: flex;
-    padding: 20px;
-    justify-content: space-between;
-    ${mobile({ padding: "0px", flexDirection: "column" })};
-`;
 
 const Categories = () => {
     return <Container>
--- src/components/Product/Products.jsx
+++ src/components/Product/Products.jsx
@@ -2,12 +2,7 @@ import styled from "styled-components"
 import { popularProducts } from "../data"
 import Product from "/Product";
 
-const Container = styled.div`
-  padding: 20px;
-  display: flex;
-  flex-wrap: wrap;
-  justify-content: space-between;
-`;
+
 
 const Products = () => {
   return (
