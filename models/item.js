877e5939e4f9d407a0d3a8b616bf1e5c5f584708
sophiabest <bestsophia1014@gmail.com>
2022-06-06 19:39:53 -0400

    model change

--- models/item.js
+++ models/item.js
@@ -1,9 +1,50 @@
 const mongoose = require('mongoose');
 
-// Ensure that the Category model is loaded in Mongoose
-require('./category');
+const itemSchema = new mongoose.Schema(
+    {
+        name: { 
+            type: String, 
+            required: 'Name is required',
+            unique: true
+        }, 
+        image: {
+            type: String,
+            require: 'Image is required'
+        },
+        brand: { 
+            type: String, 
+            required: 'Brand is required'
+        }, 
+        category: {
+            type: String,
+            required: 'Category is required'
+        },
+        description: {
+            type: String,
+            required: 'Description is required'
+        },
+        price: { 
+            type: Number, 
+            required: 'Price is required'
+        }, 
+        countInStock: { 
+            type: Number, 
+            required: 'CountInStock is required'
+        }, 
+        rating: { 
+            type: Number, 
+            required: 'Rating is required'
+        }, 
+        numReviews: { 
+            type: Number, 
+            required: 'Number of Review is required'
+        }, 
+    }, 
+    {
+        timestamps: true
+    }
+);
 
-// We want to re-use the itemSchema
-const itemSchema = require('./itemSchema');
+const Item = mongoose.model('Item', itemSchema);
 
-module.exports = mongoose.model('Item', itemSchema);
+module.exports = Item;
