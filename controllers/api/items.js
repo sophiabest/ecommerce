4a01da12ef4e164aedd9f2e353256ead3d89a2db
sophiabest <bestsophia1014@gmail.com>
2022-06-06 17:01:42 -0400

    fix items controller

--- controllers/api/items.js
+++ controllers/api/items.js
@@ -1,24 +1,18 @@
 const Item = require('../../models/item');
 
-module.exports.get_items = (req,res) => {
-    Item.find().sort({date:-1}).then(items => res.json(items));
+module.exports = {
+  index,
+  show
+};
+
+async function index(req, res) {
+  const items = await Item.find({}).sort('name').populate('category').exec();
+  // re-sort based upon the sortOrder of the categories
+  items.sort((a, b) => a.category.sortOrder - b.category.sortOrder);
+  res.json(items);
 }
 
-module.exports.post_item = (req,res) => {
-    const newItem = new Item(req.body);
-    newItem.save().then(item => res.json(item));
-}
-
-module.exports.update_item = (req,res) => {
-    Item.findByIdAndUpdate({_id: req.params.id},req.body).then(function(item){
-        Item.findOne({_id: req.params.id}).then(function(item){
-            res.json(item);
-        });
-    });
-}
-
-module.exports.delete_item = (req,res) => {
-    Item.findByIdAndDelete({_id: req.params.id}).then(function(item){
-        res.json({success: true});
-    });
+async function show(req, res) {
+  const item = await Item.findById(req.params.id);
+  res.json(item);
 }
