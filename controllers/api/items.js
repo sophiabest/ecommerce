1b5af2efc24a6ac9b4132e9e25f17cfb4e9d66a5
sophiabest <bestsophia1014@gmail.com>
2022-06-06 18:31:54 -0400

    add component pages

--- controllers/api/items.js
+++ controllers/api/items.js
@@ -1,18 +1,61 @@
 const Item = require('../../models/item');
+var fetch = require("node-fetch");
+const item = require('../../models/itemSchema');
+const API_KEY = process.env.API_KEY;
 
 module.exports = {
+  search,
+  addItem,
   index,
-  show
-};
+  getListings,
+  updateListings,
+  deleteListings
+}
 
-async function index(req, res) {
-  const items = await Item.find({}).sort('name').populate('category').exec();
-  // re-sort based upon the sortOrder of the categories
-  items.sort((a, b) => a.category.sortOrder - b.category.sortOrder);
+async function deleteListings(req, res) {
+  const items = await Item.findByIdAndDelete(req.body.id);
   res.json(items);
 }
 
-async function show(req, res) {
-  const item = await Item.findById(req.params.id);
+async function updateListings(req, res) {
+  const item = await Item.findByIdAndUpdate(req.body.id, req.body.item, { new: true });
   res.json(item);
 }
+
+async function getListings(req, res) {
+  const items = await Item.find({ user: req.user._id }).exec();
+  res.json(items);
+}
+
+
+async function index(req, res) {
+  const items = await Item.find({}).exec();
+  res.json(items);
+}
+
+async function addItem(req, res) {
+  try {
+    req.body.user = req.user._id;
+    const item = await Item.create(req.body.volumeInfo);
+    res.json(item);
+
+  } catch (err) {
+    res.status(400).json(err);
+  }
+}
+
+async function search(req, res) {
+  const response = await fetch(
+    `https://www.googleapis.com/books/v1/volumes?q=${req.body.query}&printType=books&key=${API_KEY}`
+  )
+    .then(res => {
+      if (res.ok) return res.json()
+      throw new Error('bad call')
+    })
+    .then(books => {
+      res.json(books)
+    })
+    .catch(err => {
+      res.status(400).json(err);
+    });
+};
