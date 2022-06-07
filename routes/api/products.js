cb86d5ffb0e8c4d73b609a90051846f049da4d83
sophiabest <bestsophia1014@gmail.com>
2022-06-07 14:19:29 -0400

    route

---
+++ routes/api/products.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const router = express.Router();
+const ensureLoggedIn = require('../../config/ensureLoggedIn')
+const productsCtrl = require('../../controllers/api/products');
+
+router.use(ensureLoggedIn);
+
+router.post('/search', productsCtrl.search);
+router.post('/add', productsCtrl.addBook);
+router.get('/', productsCtrl.index);
+router.get('/listings', productsCtrl.getListings);
+router.put('/edit', productsCtrl.updateListings);
+router.delete('/delete', productsCtrl.deleteListings);
+
+module.exports = router;
