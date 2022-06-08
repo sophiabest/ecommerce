cbddcc9bd2d0bcfc8440ec9c9e3c87d891be5160
sophiabest <bestsophia1014@gmail.com>
2022-06-08 15:47:49 -0400

    beofre deploy

--- controllers/api/products.js
+++ controllers/api/products.js
@@ -1,6 +1,6 @@
 const Product = require('../../models/product');
-// var fetch = require("node-fetch");
 const product = require('../../models/productSchema');
+
 
 module.exports = {
   addProduct,
@@ -31,27 +31,7 @@ async function index(req, res) {
 }
 
 async function addProduct(req, res) {
-  try {
-    req.body.user = req.user._id;
-    const product = await Product.create(req.body.volumeInfo);
-    res.json(product);
-  } catch (err) {
-    res.status(400).json(err);
-  }
+  const newProduct = await product.create(req.body);
+  res.json(newProduct);
 }
 
-async function search(req, res) {
-  const response = await fetch(
-    `'https://asos2.p.rapidapi.com/products/v2/list?store=US&offset=0&categoryId=4209&limit=48&country=US&sort=freshness&currency=USD&sizeSchema=US&lang=en-US', options`
-  )
-    .then(res => {
-      if (res.ok) return res.json()
-      throw new Error('bad call')
-    })
-    .then(products => {
-      res.json(products)
-    })
-    .catch(err => {
-      res.status(400).json(err);
-    });
-};
