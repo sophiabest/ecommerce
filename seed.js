7aef41f8947d0ac0946764fc562be57a05e6c480
sophiabest <bestsophia1014@gmail.com>
2022-06-09 19:12:50 -0400

    update seed

--- seed.js
+++ seed.js
@@ -122,7 +122,6 @@ const Product = require('./models/product');
         size: '',
         description: "Ethereal maxi dress featuring embroidered maisonette mesh and a detachable maxi skirt with tiered ruffles. Tiered ruffles at neckline and mini skirt. Open back with delicate ties. Invisible zipper at side."
       },
-      // Snares
       {
         name: 'Sadie Corset Crop Top', 
         img: 'https://cdn.shopify.com/s/files/1/0271/4967/products/210821_BC_FLL_SPRING22_RTW_LOOK_114_012_1200x.jpg?v=1646688501', 
@@ -162,13 +161,138 @@ const Product = require('./models/product');
       {
         name: 'Tiro 21 Track Pants', 
         img: 'https://assets.adidas.com/images/h_840,f_auto,q_auto,fl_lossy,c_fill,g_auto/9c7058d8677742ac8519ac3f009cdcf4_9366/Tiro_21_Track_Pants_Black_GH7305_21_model.jpg', 
-        category: categories[1], 
-        brand: brands[11], 
+        category: categories[11], 
+        brand: brands[4], 
         price: 50.00,
         size: '',
         description: "Tapered track pants made with recycled materials."
       },
-      
+      {
+        name: 'lululemon Align High-Rise Pant', 
+        img: 'https://images.lululemon.com/is/image/lululemon/LW5DRJS_038292_1?wid=1125&op_usm=0.5,2,10,0&fmt=webp&qlt=80,1&fit=constrain,0&op_sharpen=0&resMode=sharp2&iccEmbed=0&printRes=72', 
+        category: categories[9], 
+        brand: brands[5], 
+        price: 120.00,
+        size: '',
+        description: "Designed for yoga, soft and weightless fabric, high-rise."
+      },
+      {
+        name: 'Out From Under Modern Love Corset', 
+        img: 'https://images.urbndata.com/is/image/UrbanOutfitters/57537995_002_b?$xlarge$&fit=constrain&fmt=webp&qlt=80&wid=960', 
+        category: categories[1], 
+        brand: brands[6], 
+        price: 59.00,
+        size: '',
+        description: "Irresistible Out From Under corset made from a body-hugging stretch fabric with a fitted bodice and underwire cups. Decorated with lace detailing for a romantic finish and topped with adjustable spaghetti straps and hook-and-eye back closures."
+      },
+      {
+        name: 'Tropical Terrycloth Embroidered Cami', 
+        img: 'https://images.urbndata.com/is/image/UrbanOutfitters/69057198_011_b?$xlarge$&fit=constrain&fmt=webp&qlt=80&wid=960', 
+        category: categories[1], 
+        brand: brands[6], 
+        price: 29.00,
+        size: '',
+        description: "Plush terrycloth cami with embroidery at the front. Ultra-cropped with a slim fit and a scooped neckline. Topped with spaghetti straps. Only available at Urban Outfitters."
+      },
+      {
+        name: 'BDG High-Waisted Baggy Corduroy Pant - Tie Dye', 
+        img: 'https://images.urbndata.com/is/image/UrbanOutfitters/65329252_040_b?$xlarge$&fit=constrain&fmt=webp&qlt=80&wid=960', 
+        category: categories[10], 
+        brand: brands[6], 
+        price: 79.00,
+        size: '',
+        description: "A pair of soft cotton corduroy pants from BDG with a subtle tie-dye effect. High-waisted cut with a relaxed fit through the baggy straight leg. Zip fly. UO exclusive."
+      },
+      {
+        name: 'UO Madilyn Bustier Midi Dress', 
+        img: 'https://images.urbndata.com/is/image/UrbanOutfitters/67620674_030_b?$xlarge$&fit=constrain&fmt=webp&qlt=80&wid=960', 
+        category: categories[0], 
+        brand: brands[6], 
+        price: 89.00,
+        size: '',
+        description: 'UO midi dress with a bustier-inspired bodice with ruched and underwire cups. Tied spaghetti straps with a sweetheart neckline plus a slim midi skirt. Complete with a zip closure and a split hem at the back. '
+      },
+      {
+        name: 'Levi 501 Mid-Thigh Denim Short', 
+        img: 'https://images.urbndata.com/is/image/UrbanOutfitters/67891945_093_b?$xlarge$&fit=constrain&fmt=webp&qlt=80&wid=960', 
+        category: categories[2], 
+        brand: brands[9], 
+        price: 69.00,
+        size: '',
+        description: "Classic denim shorts from Levi’s with a longer inseam. High-waisted style with a relaxed leg and a cutoff hem. Zip fly and 5-pocket styling. Complete with a logo patch at the back."
+      },
+      {
+        name: 'UO Harley Linen Backless Overall', 
+        img: 'https://images.urbndata.com/is/image/UrbanOutfitters/67130559_001_b?$xlarge$&fit=constrain&fmt=webp&qlt=80&wid=960', 
+        category: categories[6], 
+        brand: brands[6], 
+        price: 70.00,
+        size: '',
+        description: "An effortless pair of overalls in a lightweight linen blend. Straight-across neckline and a wide leg. Complete with adjustable tied straps and an open back."
+      },
+      {
+        name: 'BDG High & Wide Corduroy Pant - Tan', 
+        img: 'https://images.urbndata.com/is/image/UrbanOutfitters/58524729_011_b?$xlarge$&fit=constrain&fmt=webp&qlt=80&wid=960', 
+        category: categories[10], 
+        brand: brands[6], 
+        price: 69.00,
+        size: '',
+        description: "BDG’s retro high-and-wide jean now in a cozy corduroy construction. Sits high at the waist with a relaxed fit through the wide leg silhouette. Complete with a zipper fly and 5-pocket styling."
+      },
+      {
+        name: 'UO Blake Linen Mini Dress', 
+        img: 'https://images.urbndata.com/is/image/UrbanOutfitters/68214071_010_b?$xlarge$&fit=constrain&fmt=webp&qlt=80&wid=960', 
+        category: categories[0], 
+        brand: brands[6], 
+        price: 69.00,
+        size: '',
+        description: "Effortless mini dress from UO in a lightweight linen blend. Relaxed fit with a scooped neckline, an empire waist and a flowy mini skirt. Finished with a ruched bust and an adjustable tie at the back. "
+      },
+      {
+        name: 'Converse Chuck 70 Seasonal Color High Top Sneaker', 
+        img: 'https://images.urbndata.com/is/image/UrbanOutfitters/66059130_040_b?$xlarge$&fit=constrain&fmt=webp&qlt=80&wid=960', 
+        category: categories[12], 
+        brand: brands[6], 
+        price: 85.00,
+        size: '',
+        description: 'Dreamy linen corset crop top featuring eyelet embroidered cut-out details and includes a built-in peekaboo lace bra. Delicate adjustable straps, plunging open back with a hook & eye closure.'
+      },
+      {
+        name: 'Pink Floyd 1977 Tour Crew Neck Sweatshirt', 
+        img: 'https://images.urbndata.com/is/image/UrbanOutfitters/64329840_029_b?$xlarge$&fit=constrain&fmt=webp&qlt=80&wid=960', 
+        category: categories[5], 
+        brand: brands[6], 
+        price: 79.00,
+        size: '',
+        description: 'Washed soft and faded with an overdyed finish, this pullover sweatshirt feels like a favorite vintage find. Cut long and loose in a slouchy fit with Pink Floyd graphics.'
+      },
+      {
+        name: 'Urban Renewal Recycled Low Rise Leather Mini Skirt', 
+        img: 'https://images.urbndata.com/is/image/UrbanOutfitters/69522233_001_b?$xlarge$&fit=constrain&fmt=webp&qlt=80&wid=960', 
+        category: categories[7], 
+        brand: brands[6], 
+        price: 59.00,
+        size: '',
+        description: "Updated garmet made modern this mini length skirt was created by Urban Renewal from unique vintage and deadstock items found around the world. Though similar in style to what you see here, each upcycled item is one-of-a-kind and may vary from piece to piece."
+      },
+      {
+        name: 'Basquiat Graphic Hoodie Sweatshirt', 
+        img: 'https://images.urbndata.com/is/image/UrbanOutfitters/69006237_050_e?$xlarge$&fit=constrain&fmt=webp&qlt=80&wid=960', 
+        category: categories[5], 
+        brand: brands[6], 
+        price: 79.00,
+        size: '',
+        description: 'Sweatshirt with Basquiat graphics throughout in an overdyed finish for a worn-in feel. Relaxed fit with a cozy hood, dropped shoulders and a front pocket. Complete with ribbed trim.'
+      },
+      {
+        name: 'New Balance Renew Sweatpant', 
+        img: 'https://images.urbndata.com/is/image/UrbanOutfitters/64886161_016_b?$xlarge$&fit=constrain&fmt=webp&qlt=80&wid=960', 
+        category: categories[11], 
+        brand: brands[6], 
+        price: 70.00,
+        size: '',
+        description: "Paneled sweatpants by New Balance in 100% cotton with a brushed inner. Relaxed and tapered style with pockets. Fitted with an elastic waist and gathered ankle cuffs. Complete with an adjustable drawstring."
+      }, 
     ]);
   
 
